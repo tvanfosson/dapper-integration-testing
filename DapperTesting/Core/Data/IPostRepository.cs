31fb5a1a9c0d7d5e95c3712910b42058e58404ea
Tim VanFosson <tvanfosson@gmail.com>
2014-01-01 21:32:46 -0600

    Add post repository interface.

---
+++ DapperTesting/Core/Data/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DapperTesting.Core.Model;
+
+namespace DapperTesting.Core.Data
+{
+    public interface IPostRepository
+    {
+        void Create(Post post);
+        void AddDetails(int postId, PostDetails details);
+        void Delete(int postId);
+        void DeleteDetails(int detailsId);
+        List<Post> GetPostsForUser(int userId);
+        Post Get(int id);
+        PostDetails GetDetails(int postId, int sequence);
+        void Update(Post post);
+        void UpdateDetails(PostDetails details);
+    }
+}
