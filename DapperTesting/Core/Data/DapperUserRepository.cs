850a200a110cf0d8e6a53f350b8250512ba71d5f
Tim VanFosson <tvanfosson@gmail.com>
2014-01-01 21:47:40 -0600

    Add stub implementation of user repository

---
+++ DapperTesting/Core/Data/DapperUserRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using DapperTesting.Core.Model;
+
+namespace DapperTesting.Core.Data
+{
+    public class DapperUserRepository : DapperRepositoryBase, IUserRepository
+    {
+        public DapperUserRepository(IConnectionFactory connectionFactory, string connectionStringName) 
+            : base(connectionFactory, connectionStringName)
+        {
+        }
+
+        public void Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
